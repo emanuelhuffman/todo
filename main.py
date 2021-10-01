a324cca8769bc5f702ba0c8a4d417418bc5c910c
Emanuel Huffman <ehvdb@mst.edu>
2021-09-30 21:26:13 -0500

    Added load and save

--- main.py
+++ main.py
@@ -1,59 +1,38 @@
 from tkinter import *
 from tkinter import messagebox
+import pickle
 
 task_list = []
 
-visibility = False
+def loadTasks():
+    tasks = pickle.load(open("tasks.dat", "rb"))
+    lb.delete(0, "end")
+    for task in tasks:
+        lb.insert("end", task)
+
+def saveTasks():
+    tasks = lb.get(0, lb.size())
+    pickle.dump(tasks, open("tasks.dat", "wb"))
 
 def newTask():
-    global visibility
     task = my_entry.get()
     if task != "":
-        if visibility:
-            log_lb.insert(END, task)
-        else:
-            lb.insert(END, task)
+        lb.insert(END, task)
         my_entry.delete(0, "end")
     else:
         messagebox.showwarning("warning", "Please enter some task.")
 
 def deleteTask():
-    global visibility
-    if visibility:
-        log_lb.delete(ANCHOR)
-    else:
-        lb.delete(ANCHOR)
-
-def showLog():
-    global visibility
-    if visibility:
-        log_lb.pack_forget()
-        lb.pack()
-        visibility = False
-    else:
-        visibility = True
-        lb.pack_forget()
-        log_lb.pack(side=LEFT, fill=BOTH)
+    lb.delete(ANCHOR)
 
 ws = Tk()
-ws.geometry('500x450+500+200')
+#ws.geometry('500x450+500+200')
 ws.title('Todo')
 ws.config(bg='#223441')
 ws.resizable(width=False, height=False)
 
 frame = Frame(ws)
-frame.pack(pady=10)
-
-log_lb = Listbox(
-    frame,
-    width=25,
-    height=8,
-    font=('Times', 18),
-    bd=0,
-    fg='#464646',
-    highlightthickness=0,
-    selectbackground='#a6a6a6',
-    activestyle="none",)
+frame.pack()
 
 lb = Listbox(
     frame,
@@ -77,15 +56,12 @@ sb.pack(side=RIGHT, fill=BOTH)
 lb.config(yscrollcommand=sb.set)
 sb.config(command=lb.yview)
 
-log_lb.config(yscrollcommand=sb.set)
-sb.config(command=log_lb.yview)
-
 my_entry = Entry(
     ws,
     font=('times', 24)
     )
 
-my_entry.pack(pady=20)
+my_entry.pack(pady=10, padx=10)
 
 button_frame = Frame(ws)
 button_frame.pack(pady=20)
@@ -112,15 +88,26 @@ delTask_btn = Button(
 )
 delTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
 
-log_btn = Button(
+saveTasks_btn = Button(
     button_frame,
-    text='Log',
+    text='Save Tasks',
     font=('times 14'),
-    bg='#ADF8FF',
-    padx=10,
+    bg='#118b61',
+    padx=20,
     pady=10,
-    command=showLog
+    command=saveTasks
 )
-log_btn.pack(fill=BOTH, expand=True, side=LEFT)
+saveTasks_btn.pack(fill=BOTH, expand=True, side=LEFT)
+
+loadTasks_btn = Button(
+    button_frame,
+    text='Load Tasks',
+    font=('times 14'),
+    bg='#ff8b02',
+    padx=20,
+    pady=10,
+    command=loadTasks
+)
+loadTasks_btn.pack(fill=BOTH, expand=True, side=LEFT)
 
 ws.mainloop()
